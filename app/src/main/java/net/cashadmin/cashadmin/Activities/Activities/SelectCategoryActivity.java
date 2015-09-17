a96aadc5475663f93fa16ba3f4cb498e3b4f14e1
jeremyduchesne <duchesnejeremy@gmail.com>
2015-09-17 02:50:49 +0200

    SelectCategoryActivity

--- app/src/main/java/net/cashadmin/cashadmin/Activities/Activities/SelectCategoryActivity.java
+++ app/src/main/java/net/cashadmin/cashadmin/Activities/Activities/SelectCategoryActivity.java
@@ -1,17 +1,30 @@
 package net.cashadmin.cashadmin.Activities.Activities;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 import net.cashadmin.cashadmin.Activities.Adapter.ButtonCategoryAdapter;
 import net.cashadmin.cashadmin.Activities.Database.DataManager;
 import net.cashadmin.cashadmin.Activities.Exception.IllegalTypeException;
+import net.cashadmin.cashadmin.Activities.Model.Category;
 import net.cashadmin.cashadmin.Activities.Model.Entity;
 import net.cashadmin.cashadmin.Activities.Model.Enum.TypeEnum;
 import net.cashadmin.cashadmin.R;
@@ -20,11 +33,23 @@ import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class SelectCategoryActivity extends ActionBarActivity {
 
     @InjectView(R.id.gridView)
     GridView mGridView;
+    @InjectView(R.id.addCategoryButton)
+    ImageButton mAddCategoryButton;
+    @InjectView(R.id.colorChoice)
+    TextView mColorChoice;
+    @InjectView(R.id.addFinalCategoryButton)
+    Button mAddFinalCategoryButton;
+    @InjectView(R.id.nameCat)
+    EditText mNameCat;
+
+    private int color;
+    private DataManager mDataManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,5 +84,49 @@ public class SelectCategoryActivity extends ActionBarActivity {
                                              }
                                          }
         );
+    }
+
+    @OnClick(R.id.addCategoryButton)
+    public void onClickAddCategoryButton() {
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        PopupWindow pop = new PopupWindow(getLayoutInflater().inflate(R.layout.new_category_popup,null));
+        pop.setAnimationStyle(-1);
+    }
+
+    @OnClick(R.id.colorChoice)
+    public void onClickColorChoice(){
+        ColorPickerDialogBuilder
+                .with(this)
+                .setTitle(getString(R.string.ChooseColor))
+                .initialColor(R.color.White)
+                .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
+                .density(12)
+                .setPositiveButton("Ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, Integer[] integers) {
+                        mColorChoice.setBackgroundColor(i);
+                        color = i;
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+    }
+
+    @OnClick(R.id.addFinalCategoryButton)
+    public void onClickAddFinalCategoryButton(){
+        String name = mNameCat.getText().toString().trim();
+
+        //TODO : Faire un popup si name est vide
+
+        Category cat = new Category(0, name, color);
+        mDataManager.insert(cat);
+        Intent intent = new Intent(SelectCategoryActivity.this, NewExpenseActivity.class);
+        intent.putExtra("catName", name);
+        startActivity(intent);
     }
 }
