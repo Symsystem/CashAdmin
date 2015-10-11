a408546695c9d9345c628ef397ef75e5d37db3bb
Symsystem <symeon.delmarmol@gmail.com>
2015-10-12 00:27:55 +0200

    Delete on historicActivity + animation

--- app/src/main/java/net/cashadmin/cashadmin/Activities/Activities/HistoricActivity.java
+++ app/src/main/java/net/cashadmin/cashadmin/Activities/Activities/HistoricActivity.java
@@ -2,6 +2,8 @@ package net.cashadmin.cashadmin.Activities.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ListView;
 
 import net.cashadmin.cashadmin.Activities.Adapter.TransactionHistoryAdapter;
@@ -13,6 +15,7 @@ import net.cashadmin.cashadmin.Activities.Model.Transaction;
 import net.cashadmin.cashadmin.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -20,7 +23,11 @@ import butterknife.InjectView;
 
 public class HistoricActivity extends AppCompatActivity {
 
+    private static final int MOVE_DURATION = 250;
+
     private DataManager mDataManager;
+    private TransactionHistoryAdapter mAdapter;
+    private HashMap<Long, Integer> mItemIdTopMap = new HashMap<>();
 
     @InjectView(R.id.historyList)
     ListView mHistoryList;
@@ -37,11 +44,82 @@ public class HistoricActivity extends AppCompatActivity {
                     mDataManager.getAll(TypeEnum.EXPENSE),
                     mDataManager.getAll(TypeEnum.INCOME));
 
-            TransactionHistoryAdapter transactionAdapter = new TransactionHistoryAdapter(HistoricActivity.this, transactions);
-            mHistoryList.setAdapter(transactionAdapter);
+            mAdapter = new TransactionHistoryAdapter(HistoricActivity.this, transactions, mOnClickDeleteListener);
+            mHistoryList.setAdapter(mAdapter);
         } catch (IllegalTypeException e) {
             e.printStackTrace();
         }
+    }
+
+    private View.OnClickListener mOnClickDeleteListener = new View.OnClickListener() {
+        @Override
+        public void onClick(final View view) {
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    animateRemoval(mHistoryList, view);
+                }
+            };
+            r.run();
+        }
+    };
+
+    /**
+     * This method animates all other views in the ListView container (not including ignoreView)
+     * into their final positions. It is called after ignoreView has been removed from the
+     * adapter, but before layout has been run. The approach here is to figure out where
+     * everything is now, then allow layout to run, then figure out where everything is after
+     * layout, and then to run animations between all of those start/end positions.
+     */
+    private void animateRemoval(final ListView listview, View viewToRemove) {
+        int firstVisiblePosition = listview.getFirstVisiblePosition();
+        for (int i = 0; i < listview.getChildCount(); ++i) {
+            View child = listview.getChildAt(i);
+            if (child != viewToRemove) {
+                int position = firstVisiblePosition + i;
+                long itemId = mAdapter.getItemId(position);
+                mItemIdTopMap.put(itemId, child.getTop());
+            }
+        }
+        // Delete the item from the adapter
+        final int position = listview.getPositionForView(viewToRemove);
+        Transaction t = mAdapter.getItem(position);
+        mDataManager.delete(t);
+        mAdapter.remove(t);
+
+        final ViewTreeObserver observer = listview.getViewTreeObserver();
+        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            public boolean onPreDraw() {
+                observer.removeOnPreDrawListener(this);
+                boolean firstAnimation = true;
+                int firstVisiblePosition = listview.getFirstVisiblePosition();
+                for (int i = position; i < listview.getChildCount(); ++i) {
+                    final View child = listview.getChildAt(i);
+                    int position = firstVisiblePosition + i;
+                    long itemId = mAdapter.getItemId(position);
+                    Integer startTop = mItemIdTopMap.get(itemId);
+                    int top = child.getTop();
+                    if (startTop != null) {
+                        if (startTop != top) {
+                            int delta = startTop - top;
+                            child.setTranslationY(delta);
+                            child.animate().setDuration(MOVE_DURATION).translationY(0);
+                        }
+                    } else {
+                        // Animate new views along with the others. The catch is that they did not
+                        // exist in the start state, so we must calculate their starting position
+                        // based on neighboring views.
+                        int childHeight = child.getHeight() + listview.getDividerHeight();
+                        startTop = top + (i > 0 ? childHeight : -childHeight);
+                        int delta = startTop - top;
+                        child.setTranslationY(delta);
+                        child.animate().setDuration(MOVE_DURATION).translationY(0);
+                    }
+                }
+                mItemIdTopMap.clear();
+                return true;
+            }
+        });
     }
 
     private ArrayList<Transaction> mergeExpenseIncome(List<Entity> l1, List<Entity> l2){
--- app/src/main/java/net/cashadmin/cashadmin/Activities/Adapter/TransactionHistoryAdapter.java
+++ app/src/main/java/net/cashadmin/cashadmin/Activities/Adapter/TransactionHistoryAdapter.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daimajia.swipe.SwipeLayout;
 
@@ -14,14 +16,32 @@ import net.cashadmin.cashadmin.Activities.Model.Transaction;
 import net.cashadmin.cashadmin.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TransactionHistoryAdapter extends ArrayAdapter<Transaction> {
 
+    private HashMap<Transaction, Integer> mIdMap = new HashMap<Transaction, Integer>();
     private ArrayList<Transaction> mTransactions;
+    private View.OnClickListener mOnClickDeleteListener;
 
-    public TransactionHistoryAdapter(Context context, ArrayList<Transaction> objects) {
+    public TransactionHistoryAdapter(Context context, ArrayList<Transaction> objects, View.OnClickListener onClickDeleteListener) {
         super(context, R.layout.historic_item, objects);
+        for(int i = 0; i < objects.size(); ++i){
+            mIdMap.put(objects.get(i), i);
+        }
         mTransactions = objects;
+        mOnClickDeleteListener = onClickDeleteListener;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        Transaction item = getItem(position);
+        return mIdMap.get(item);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
     }
 
     @Override
@@ -34,19 +54,21 @@ public class TransactionHistoryAdapter extends ArrayAdapter<Transaction> {
             holder = new Holder();
             holder.mSwipeLayout = (SwipeLayout) convertView.findViewById(R.id.swipeHistoricLayout);
             holder.mSwipeLayout.setShowMode(SwipeLayout.ShowMode.PullOut);
+            holder.mEditButton = (RelativeLayout) convertView.findViewById(R.id.editItemButton);
+            holder.mDeleteButton = (RelativeLayout) convertView.findViewById(R.id.deleteItemButton);
             holder.mColorCategory = (View) convertView.findViewById(R.id.colored_bar);
             holder.mLabel = (TextView) convertView.findViewById(R.id.label);
             holder.mCategory = (TextView) convertView.findViewById(R.id.category);
             holder.mAmount = (TextView) convertView.findViewById(R.id.amount);
 
             convertView.setTag(holder);
-        }
-        else{
+
+        } else {
             holder = (Holder) convertView.getTag();
         }
 
-        Transaction t = mTransactions.get(position);
-        switch(t.getType()){
+        final Transaction t = mTransactions.get(position);
+        switch (t.getType()) {
             case EXPENSE:
                 Expense e = (Expense) t;
                 holder.mCategory.setText(e.getCategory().getLabel());
@@ -63,11 +85,22 @@ public class TransactionHistoryAdapter extends ArrayAdapter<Transaction> {
         }
         holder.mLabel.setText(t.getLabel());
 
+        holder.mEditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(TransactionHistoryAdapter.this.getContext(), "EDIT", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        holder.mDeleteButton.setOnClickListener(mOnClickDeleteListener);
+
         return convertView;
     }
 
-    private static class Holder{
+    private static class Holder {
         private SwipeLayout mSwipeLayout;
+        private RelativeLayout mEditButton;
+        private RelativeLayout mDeleteButton;
         private View mColorCategory;
         private TextView mLabel;
         private TextView mCategory;
