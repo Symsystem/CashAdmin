c046d246309917566414d6f3745eb054df584ad1
Symeon del Marmol <symeon.delmarmol@gmail.com>
2016-02-17 13:59:11 +0100

    Extériorisation de la methode animateRemoval

--- app/src/main/java/net/cashadmin/cashadmin/Activities/Activities/HistoricActivity.java
+++ app/src/main/java/net/cashadmin/cashadmin/Activities/Activities/HistoricActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
@@ -19,18 +18,16 @@ import net.cashadmin.cashadmin.Activities.Model.Enum.TransactionEntryEnum;
 import net.cashadmin.cashadmin.Activities.Model.Expense;
 import net.cashadmin.cashadmin.Activities.Model.Income;
 import net.cashadmin.cashadmin.Activities.Model.Transaction;
+import net.cashadmin.cashadmin.Activities.Utils.Utils;
 import net.cashadmin.cashadmin.R;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class HistoricActivity extends AppCompatActivity {
-
-    private static final int MOVE_DURATION = 250;
 
     private DataManager mDataManager;
     private TransactionHistoryAdapter mAdapter;
@@ -103,7 +100,7 @@ public class HistoricActivity extends AppCompatActivity {
             Runnable r = new Runnable() {
                 @Override
                 public void run() {
-                    animateRemoval(mHistoryList, view);
+                    Utils.animateRemoval(mHistoryList, view, mAdapter, mHistoryList);
 
                     // Delete the item from the adapter
                     Transaction t = mAdapter.getItem(mHistoryList.getPositionForView(view));
@@ -148,60 +145,6 @@ public class HistoricActivity extends AppCompatActivity {
             else
                 mCheckExpense.setChecked(true);
         }
-    }
-
-    /**
-     * This method animates all other views in the ListView container (not including ignoreView)
-     * into their final positions. It is called after ignoreView has been removed from the
-     * adapter, but before layout has been run. The approach here is to figure out where
-     * everything is now, then allow layout to run, then figure out where everything is after
-     * layout, and then to run animations between all of those start/end positions.
-     */
-    private void animateRemoval(final ListView listview, View viewToRemove) {
-        final HashMap<Long, Integer> itemIdTopMap = new HashMap<>(); // Map pour connaitre les
-        int firstVisiblePosition = listview.getFirstVisiblePosition();
-        for (int i = 0; i < listview.getChildCount(); ++i) {
-            View child = listview.getChildAt(i);
-            if (child != viewToRemove) {
-                int position = firstVisiblePosition + i;
-                long itemId = mAdapter.getItemId(position);
-                itemIdTopMap.put(itemId, child.getTop());
-            }
-        }
-
-        final int position = mHistoryList.getPositionForView(viewToRemove);
-        final ViewTreeObserver observer = listview.getViewTreeObserver();
-        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            public boolean onPreDraw() {
-                observer.removeOnPreDrawListener(this);
-                int firstVisiblePosition = listview.getFirstVisiblePosition();
-                for (int i = position; i < listview.getChildCount(); ++i) {
-                    final View child = listview.getChildAt(i);
-                    int position = firstVisiblePosition + i;
-                    long itemId = mAdapter.getItemId(position);
-                    Integer startTop = itemIdTopMap.get(itemId);
-                    int top = child.getTop();
-                    if (startTop != null) {
-                        if (startTop != top) {
-                            int delta = startTop - top;
-                            child.setTranslationY(delta);
-                            child.animate().setDuration(MOVE_DURATION).translationY(0);
-                        }
-                    } else {
-                        // Animate new views along with the others. The catch is that they did not
-                        // exist in the start state, so we must calculate their starting position
-                        // based on neighboring views.
-                        int childHeight = child.getHeight() + listview.getDividerHeight();
-                        startTop = top + (i > 0 ? childHeight : -childHeight);
-                        int delta = startTop - top;
-                        child.setTranslationY(delta);
-                        child.animate().setDuration(MOVE_DURATION).translationY(0);
-                        break;
-                    }
-                }
-                return true;
-            }
-        });
     }
 
     private ArrayList<Transaction> mergeExpenseIncome(List<Transaction> l1, List<Transaction> l2){
---
+++ app/src/main/java/net/cashadmin/cashadmin/Activities/Utils/Utils.java
@@ -0,0 +1,67 @@
+package net.cashadmin.cashadmin.Activities.Utils;
+
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.HashMap;
+
+public class Utils {
+
+    private static final int MOVE_DURATION = 250;
+
+    /**
+     * This method animates all other views in the ListView container (not including ignoreView)
+     * into their final positions. It is called after ignoreView has been removed from the
+     * adapter, but before layout has been run. The approach here is to figure out where
+     * everything is now, then allow layout to run, then figure out where everything is after
+     * layout, and then to run animations between all of those start/end positions.
+     */
+    public static void animateRemoval(final ListView listview, View viewToRemove, final ArrayAdapter<?> adapter, ListView previousList) {
+        final HashMap<Long, Integer> itemIdTopMap = new HashMap<>(); // Map pour connaitre les
+        int firstVisiblePosition = listview.getFirstVisiblePosition();
+        for (int i = 0; i < listview.getChildCount(); ++i) {
+            View child = listview.getChildAt(i);
+            if (child != viewToRemove) {
+                int position = firstVisiblePosition + i;
+                long itemId = adapter.getItemId(position);
+                itemIdTopMap.put(itemId, child.getTop());
+            }
+        }
+
+        final int position = previousList.getPositionForView(viewToRemove);
+        final ViewTreeObserver observer = listview.getViewTreeObserver();
+        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            public boolean onPreDraw() {
+                observer.removeOnPreDrawListener(this);
+                int firstVisiblePosition = listview.getFirstVisiblePosition();
+                for (int i = position; i < listview.getChildCount(); ++i) {
+                    final View child = listview.getChildAt(i);
+                    int position = firstVisiblePosition + i;
+                    long itemId = adapter.getItemId(position);
+                    Integer startTop = itemIdTopMap.get(itemId);
+                    int top = child.getTop();
+                    if (startTop != null) {
+                        if (startTop != top) {
+                            int delta = startTop - top;
+                            child.setTranslationY(delta);
+                            child.animate().setDuration(MOVE_DURATION).translationY(0);
+                        }
+                    } else {
+                        // Animate new views along with the others. The catch is that they did not
+                        // exist in the start state, so we must calculate their starting position
+                        // based on neighboring views.
+                        int childHeight = child.getHeight() + listview.getDividerHeight();
+                        startTop = top + (i > 0 ? childHeight : -childHeight);
+                        int delta = startTop - top;
+                        child.setTranslationY(delta);
+                        child.animate().setDuration(MOVE_DURATION).translationY(0);
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+}
